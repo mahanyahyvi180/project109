c36240c6ae48d0aac903c226e64b2e2cce13f948
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-19 10:17:11 +0330

    Add files via upload

---
+++ ConsoleApp95/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp95
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int passes = 0;
+            int failures = 0;
+            int studentcounter = 1;
+
+            while (studentcounter <= 10)
+            {
+                Console.Write("Enter result(1=fail,2=pass):");
+                int result = int.Parse(Console.ReadLine());
+
+                if (result == 1)
+                {
+                    passes = passes + 1;
+                }
+                else
+                {
+                    failures = failures + 1;
+                }
+                studentcounter = studentcounter + 1;
+            }
+            Console.Write($"passed=passes\nfailed=failures");
+
+            if (passes >= 8)
+            {
+                Console.WriteLine("you win");
+                Console.ReadLine();
+            }
+        }
+    }
+}
